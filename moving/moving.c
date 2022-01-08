19349fdbfe2aa91cd4cfcd47ee4bf4c3e74f9ba8
Anas jaidi <anas.jaidi@icloud.com>
2022-01-08 21:40:29 +0100

    finish day work

--- moving/moving.c
+++ moving/moving.c
@@ -6,7 +6,7 @@
 /*   By: ajaidi <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/07 07:25:06 by ajaidi            #+#    #+#             */
-/*   Updated: 2022/01/07 07:25:06 by ajaidi           ###   ########.fr       */
+/*   Updated: 2022/01/08 21:40:13 by ajaidi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,12 +31,12 @@ int key_hook(int key, t_all *all)
 {
     if (key == 0)
         check_x(all, key);
-    // if (key == 1)
-    //     check_y(all, key);
-    // if (key == 2)
-    //     check_x(all, key);
-    // if (key == 13)
-    //     check_y(all, key);
+	if (key == 1)
+        check_y(all, key);
+    if (key == 2)
+		check_x(all, key);
+    if (key == 13)
+		check_y(all, key);
     return (0);
 }
 
